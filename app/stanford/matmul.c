28ebf88efc38f36105faadbc82664b56736212a1
yim2012 <yim2012@ec3883af-05a9-0c2b-e492-98e3f142ab6f>
2010-10-17 22:56:01 +0000

    app

---
+++ app/stanford/matmul.c
@@ -0,0 +1,58 @@
+/* matrix multiplication */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define  N  50
+
+int a[N][N], b[N][N], c[N][N];
+
+rnum() {
+  return( ((random())%10)+1 );
+}
+
+show() {
+  int i,j;
+
+  for(i=0; i<N; i++)  {
+    for(j=0; j<N; j++)
+      printf("%d ",c[i][j]);
+    printf("\n");
+  }
+}
+
+inita() {
+  int i,j;
+
+  for(i=0; i<N; i++)
+    for(j=0; j<N; j++)
+      a[i][j] = 2;
+}
+
+initb() {
+  int i,j;
+
+  for(i=0; i<N; i++)
+    for(j=0; j<N; j++)
+      b[i][j] = 3;
+}
+
+main() {
+  inita();
+  initb();
+  matmul();
+  show();
+}
+
+matmul() {
+  int i,j,k;
+
+  for(i=0; i<N; i++) {
+    for(j=0; j<N; j++) {
+      c[i][j] = 0;
+      for(k=0; k<N; k++) {
+	c[i][j] += a[i][k] * b[k][j];
+      }
+    }
+  }
+}
